d2f8e8f69d2bc846d01307bd1011d89c5a2ed0da
AlwinChong02 <alwinchong020710@gmail.com>
2025-04-04 21:42:15 +0800

    add table columns for feedbacks table

--- database/migrations/2025_04_04_130905_create_feedbacks_table.php
+++ database/migrations/2025_04_04_130905_create_feedbacks_table.php
@@ -15,6 +15,11 @@ class CreateFeedbacksTable extends Migration
     {
         Schema::create('feedbacks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('subject');
+            $table->text('description');
+            $table->string('phone')->nullable(); // might be optional
             $table->timestamps();
         });
     }
