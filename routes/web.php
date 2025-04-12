2e1d5324e99e1b2dadc7b7fedadb34c9ebf88c93
Steve119-64bits <stevechan0991@gmail.com>
2025-04-12 17:23:01 +0800

    Update web.php
    Updated route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+Route::get('/navigationbar', function () {
+    return view('navigationbar');
+});
+Route::get('/footer', function () {
+    return view('footer');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+// Route::get('/search', function () {
+//     return view('search');
+// });
+
+Route::group(['prefix' => 'searchBar', 'as' => 'searchBar.'], function ()
+{
+    Route::get('/',
+        [SearchController::class, 'index']
+    )->name('index');
+
+    Route::get('search',
+        [SearchController::class, 'search']
+    )->name('search');
+
+    Route::get('show',
+        [SearchController::class, 'show']
+    )->name('show');
+});
+// Route::get('/datatest', [Events::class, 'testData']);
+
+Route::get('page/{id}', [SearchController::class, 'pages'])->name('pages');
+
+Route::get("/contact", [ContactController::class, 'showContactPage']);
+Route::post("/contact", [ContactController::class, 'contact']);
+Route::post('/contact', [ContactController::class, 'addFeedback'])->name('contact.submit');
+
