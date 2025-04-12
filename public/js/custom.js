3195797b855010bf5a7bbe9d12d16cb16275b306
Steve119-64bits <stevechan0991@gmail.com>
2025-04-12 17:06:38 +0800

    Update custom.js
    add code which related to about page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let toggles = document.getElementsByClassName("toggle-faq");
+    let contents = document.getElementsByClassName("content-faq");
+    let icons = document.getElementsByClassName("fa");
 
+    for (let i = 0; i < toggles.length; i++) {
+        toggles[i].addEventListener("click", function () {
+            let content = contents[i];
+            let icon = icons[i];
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                toggles[i].style.color = "#111130";
+                icon.classList.remove("fa-minus");
+                icon.classList.add("fa-plus");
+            } else {
+                for (let j = 0; j < contents.length; j++) {
+                    contents[j].style.maxHeight = null;
+                    toggles[j].style.color = "#111130";
+                    icons[j].classList.remove("fa-minus");
+                    icons[j].classList.add("fa-plus");
+                }
+                content.style.maxHeight = content.scrollHeight + "px";
+                toggles[i].style.color = "#0084e9";
+                icon.classList.remove("fa-plus");
+                icon.classList.add("fa-minus");
+            }
+        });
+    }
+});
