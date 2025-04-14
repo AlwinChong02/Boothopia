bcc8e739f3918df47a707d24b4163eff1d538476
AlwinChong02 <alwinchong020710@gmail.com>
2025-04-14 16:05:39 +0800

    add booth controller

---
+++ app/Http/Controllers/BoothController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Booth;
+
+class BoothController extends Controller
+{
+    public function index()
+    {
+        $booths = Booth::all(); // Fetch all booths from the database
+        return view('booths.index', compact('booths'));
+    }
+}
