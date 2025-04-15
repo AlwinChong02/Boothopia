970fc200048163a8ac349c596f74a4e5ab0804cd
AlwinChong02 <alwinchong020710@gmail.com>
2025-04-15 22:46:34 +0800

    add functions to Booth  model

--- app/Models/Booth.php
+++ app/Models/Booth.php
@@ -14,12 +14,33 @@ class Booth extends Model
 
     public function index()
     {
-        // Fetch all booths from the database
         $booths = Booth::all();
-
-        // Pass the data to the view
         return view('booths.index', compact('booths'));
     }
 
-    
+    public function getBoothById($id) //fetch single booth ONLY
+    {
+        return Booth::findOrFail($id);
+    }
+    // Create a new booth (Create)
+    public function createBooth($data)
+    {
+        return Booth::create($data);
+    }
+
+    public function updateBooth($id, $data)
+    {
+        $booth = Booth::findOrFail($id);
+        $booth->update($data);
+        return $booth;
+    }
+    // Delete a booth (Delete)
+    public function deleteBooth($id)
+    {
+        $booth = Booth::findOrFail($id);
+        $booth->delete();
+        return true;
+    }
+
+
 }
