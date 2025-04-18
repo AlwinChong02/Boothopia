14fcd1be85939b2b8aa2e25905f335c814d373cf
AlwinChong02 <alwinchong020710@gmail.com>
2025-04-18 18:12:48 +0800

    add in database logic in paymentController

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Approval;
+
 class PaymentController extends Controller
 {
     /**
@@ -16,11 +18,23 @@ class PaymentController extends Controller
     {
         $request->validate([
             'approval_image' => 'required | image | mimes:jpeg,png,jpg,gif | max:2048',
+            'organiser_id' => 'required | integer',
+            'requester_id' => 'required | integer',
+            'status' => 'required | string',
         ]);
         $image = $request->file('approval_image');
         $imageName = time().'_'.uniqid().'.'.$image->getClientOriginalExtension();
         $image->move(public_path('img/approvals'), $imageName);
+
+        Approval::create([
+            'organiser_id' => $request->input('organiser_id'),
+            'requester_id' => $request->input('requester_id'),
+            'status' => $request->input('status'),
+            'approval_image' => 'img/approvals/'.$imageName,
+        ]);
         
         return redirect('/home')->with('success', 'Approval image uploaded successfully!');
     }
+
+    
 }
