df61383194d1e78c575951dacd2a893364be09ee
MKxian2 <kmx@hotmail.my>
2025-04-18 19:25:16 +0800

    fix bugs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,31 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
-@section('title', 'Home Page') 
-
-@if(session('success'))
-    <x-notification type="success" :message="session('success')" />
-@endif
-
-@if(session('error'))
-    <x-notification type="error" :message="session('error')" />
-@endif
+@section('title', 'Home Page')
 
 @section('content')
+    {{-- session alerts inside content --}}
     @if(session('success'))
-        <div class="alert alert-success mt-3">
-            {{ session('success') }}
-        </div>
+        <x-notification type="success" :message="session('success')" />
+    @endif
+
+    @if(session('error'))
+        <x-notification type="error" :message="session('error')" />
     @endif
 
     <button onclick="scrollToTop()">Back to Top</button>
+
     <video class="search-video" autoplay loop muted width="1600" height="1500">
         <source src="/video/roadshow.mp4" type="video/mp4">
     </video>
+
+    <h1 style="font-size: 1000px;">HIt asdasdsad</h1>
     <section class="search-section">
         <div class="search-bar">
             <div class="search-background">
-                <img src="/img/searchBackground.png" alt="Booking" class="search-background" width="1521" height="750">
+                <img src="{{ asset('img/searchBackground.png') }}" alt="Booking"
+                     class="search-background" width="1521" height="750">
             </div>
-            @include('searchBar.index') 
+            @include('searchBar.partials.search')
         </div>
     </section>
 @endsection
